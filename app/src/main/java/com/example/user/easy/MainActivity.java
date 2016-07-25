d7487ed9f1b4b51c354c8ab968ae1059c3288bbd
fanshixiang <1019547293@qq.com>
2016-07-25 14:28:43 +0800

    3

---
+++ app/src/main/java/com/example/user/easy/MainActivity.java
@@ -0,0 +1,15 @@
+package com.example.user.easy;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        //第一次提交
+        //第二次提交
+    }
+}
